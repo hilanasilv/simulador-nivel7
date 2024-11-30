e384a42a622d9b06133aab8ba0530c16f28aae3d
Nayla Hilana da Silva <hilanacs@outlook.com>
2024-11-29 21:38:36 -0300

    feat: adiciona arquivos principais do projetos

---
+++ GerenciadorBiblioteca/Program.cs
@@ -0,0 +1,36 @@
+﻿using GerenciadorBiblioteca.Controllers;
+using GerenciadorBiblioteca.Data;
+using System;
+using Unity;
+using System.Windows.Forms;
+using GerenciadorBiblioteca.IClass;
+
+
+
+namespace LibraryManager
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            var container = new UnityContainer();
+
+            // Registrar dependências
+            container.RegisterType<BibliotecaData>();
+            container.RegisterType<LibraryController>();
+
+            // Inicializar dados
+            var data = container.Resolve<BibliotecaData>();
+            data.SeedData();
+
+            // Exibe o formulário de usuário incialmente
+            var usuárioForm = container.Resolve<UsuárioForm>();
+            Application.Run(usuárioForm);
+
+            // Após fechar o UsuárioForm, exibe o GerenciadorForm
+            var gerenciadorForm = container.Resolve<GerenciadorForm>();
+            Application.Run(gerenciadorForm);
+        }
+    }
+}
