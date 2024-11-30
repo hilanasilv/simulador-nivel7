0781b8fd42950219b59d8b52b54346f546ae9a54
Nayla Hilana da Silva <hilanacs@outlook.com>
2024-11-29 21:37:54 -0300

    adiciona a interface do projeto

---
+++ GerenciadorBiblioteca/IClass/UsuárioForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace GerenciadorBiblioteca.IClass
+{
+    partial class UsuárioForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtSearch.Location = new System.Drawing.Point(293, 69);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(119, 20);
+            this.txtSearch.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(443, 69);
+            this.button1.Name = "btnSearch";
+            this.button1.Size = new System.Drawing.Size(47, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "buscar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(293, 135);
+            this.dataGridView1.Name = "dgvBooks";
+            this.dataGridView1.Size = new System.Drawing.Size(197, 214);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(351, 375);
+            this.button2.Name = "btnEmprestimo";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "empréstimo";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // UsuarioForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSearch);
+            this.Name = "UsuarioForm";
+            this.Text = "Formulário de Usuário";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ GerenciadorBiblioteca/IClass/UsuárioForm.cs
@@ -0,0 +1,38 @@
+﻿using GerenciadorBiblioteca.Controllers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GerenciadorBiblioteca.IClass
+{
+    public partial class UsuárioForm : Form
+    {
+        private readonly LibraryController _libraryController;
+
+        public UsuárioForm(LibraryController libraryController)
+        {
+            InitializeComponent();
+            _libraryController = libraryController;
+        }
+
+        private void LoadBooks()
+        {
+            var books = _libraryController.GetBooks();
+            dgvBooks.DataSource = books;
+        }
+
+        private void btnBorrowBook_Click(object sender, EventArgs e)
+        {
+            var selectedBookId = (Guid)dgvBooks.SelectedRows[0].Cells["Id"].Value;
+            _libraryController.BorrowBook(selectedBookId);
+            LoadBooks();  // Atualiza a lista de livros após emprestar
+        }
+    }
+
+}
